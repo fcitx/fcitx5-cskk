17c6b81c6733fe167bdc6157c58c5fe6c9aa207f
Naoaki Iwakiri <naokiri@gmail.com>
2022-09-03 00:18:03 +0900

    Fix spdx copyright to be multiline

--- gui/adddictdialog.cpp
+++ gui/adddictdialog.cpp
@@ -1,6 +1,6 @@
 /*
- * SPDX-FileCopyrightText: 2013~2022 CSSlayer <wengxt@gmail.com>, Naoaki Iwakiri
- * <naokiri@gmail.com>
+ * SPDX-FileCopyrightText: <text>2013~2022 CSSlayer <wengxt@gmail.com>, Naoaki Iwakiri
+ * <naokiri@gmail.com></text>
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
