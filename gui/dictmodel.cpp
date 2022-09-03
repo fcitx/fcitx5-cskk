5b45641f297d415e1b477c7c56cbf39b700b56b2
Naoaki Iwakiri <naokiri@gmail.com>
2022-09-03 12:46:17 +0900

    Edit button (#34)
    * Edit button

--- gui/dictmodel.cpp
+++ gui/dictmodel.cpp
@@ -1,193 +1,224 @@
 /*
- * SPDX-FileCopyrightText: 2013~2020 CSSlayer <wengxt@gmail.com>
+ * SPDX-FileCopyrightText: <text>2013~2022 CSSlayer <wengxt@gmail.com>, Naoaki Iwakiri
+ * <naokiri@gmail.com></text>
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
  */
 
+#include "dictmodel.h"
 #include <QDebug>
 #include <QFile>
 #include <QStringList>
 #include <QTemporaryFile>
 #include <QtGlobal>
 #include <fcitx-utils/standardpath.h>
-#include <iostream>
-
 #include <fcntl.h>
-#include "dictmodel.h"
+#include <iostream>
 
 namespace fcitx {
 
 const std::string config_path = "cskk/dictionary_list";
 
-SkkDictModel::SkkDictModel(QObject *parent) : QAbstractListModel(parent) {
-    m_knownKeys << "file"
-                << "type"
-                << "mode"
-                << "encoding";
-}
+SkkDictModel::SkkDictModel(QObject *parent) : QAbstractListModel(parent) {}
 
 void SkkDictModel::defaults() {
-    auto path =
-        StandardPath::fcitxPath("pkgdatadir", config_path.c_str());
-    QFile f(path.data());
-    if (f.open(QIODevice::ReadOnly)) {
-        load(f);
-    }
+  auto path = StandardPath::fcitxPath("pkgdatadir", config_path.c_str());
+  QFile f(path.data());
+  if (f.open(QIODevice::ReadOnly)) {
+    load(f);
+  }
 }
 
 void SkkDictModel::load() {
-    auto file = StandardPath::global().open(StandardPath::Type::PkgData,
+  auto file = StandardPath::global().open(StandardPath::Type::PkgData,
                                           config_path, O_RDONLY);
-    if (file.fd() < 0) {
-        return;
-    }
-    QFile f;
-    if (!f.open(file.fd(), QIODevice::ReadOnly)) {
-        return;
-    }
+  if (file.fd() < 0) {
+    return;
+  }
+  QFile f;
+  if (!f.open(file.fd(), QIODevice::ReadOnly)) {
+    return;
+  }
 
-    load(f);
-    f.close();
+  load(f);
+  f.close();
 }
 
 void SkkDictModel::load(QFile &file) {
-    beginResetModel();
-    m_dicts.clear();
+  beginResetModel();
+  m_dicts.clear();
 
-    QByteArray bytes;
-    while (!(bytes = file.readLine()).isEmpty()) {
-        QString line = QString::fromUtf8(bytes).trimmed();
-        QStringList items = line.split(",");
-        // No matter which type, it should has at least 3 keys.
-        if (items.size() < 3) {
-            continue;
-        }
-
-        bool failed = false;
-        QMap<QString, QString> dict;
-        Q_FOREACH (const QString &item, items) {
-            if (!item.contains('=')) {
-                failed = true;
-                break;
-            }
-            QString key = item.section('=', 0, 0);
-            QString value = item.section('=', 1, -1);
-
-            if (!m_knownKeys.contains(key)) {
-                continue;
-            }
-
-            dict[key] = value;
-        }
-
-        if (!failed && 3 <= dict.size()) {
-            m_dicts << dict;
-        }
+  QByteArray bytes;
+  while (!(bytes = file.readLine()).isEmpty()) {
+    QString line = QString::fromUtf8(bytes).trimmed();
+    QMap<QString, QString> dict = parseLine(line);
+    if (3 <= dict.size()) {
+      m_dicts << dict;
     }
-    endResetModel();
+  }
+  endResetModel();
 }
 
 bool SkkDictModel::save() {
-    return StandardPath::global().safeSave(
-        StandardPath::Type::PkgData, config_path, [this](int fd) {
-            QFile tempFile;
-            if (!tempFile.open(fd, QIODevice::WriteOnly)) {
-                return false;
-            }
+  return StandardPath::global().safeSave(
+      StandardPath::Type::PkgData, config_path, [this](int fd) {
+        QFile tempFile;
+        if (!tempFile.open(fd, QIODevice::WriteOnly)) {
+          return false;
+        }
 
-            typedef QMap<QString, QString> DictType;
+        typedef QMap<QString, QString> DictType;
 
-            Q_FOREACH (const DictType &dict, m_dicts) {
-                bool first = true;
-                Q_FOREACH (const QString &key, dict.keys()) {
-                    if (first) {
-                        first = false;
-                    } else {
-                        tempFile.write(",");
-                    }
-                    tempFile.write(key.toUtf8());
-                    tempFile.write("=");
-                    tempFile.write(dict[key].toUtf8());
-                }
-                tempFile.write("\n");
-            }
-            return true;
-        });
+        Q_FOREACH (const DictType &dict, m_dicts) {
+          QString line = serialize(dict);
+          tempFile.write(line.toUtf8());
+          tempFile.write("\n");
+        }
+        return true;
+      });
 }
 
 int SkkDictModel::rowCount(const QModelIndex &parent) const {
-    if (parent.isValid()) {
-        return 0;
-    }
-    return m_dicts.size();
+  if (parent.isValid()) {
+    return 0;
+  }
+  return m_dicts.size();
 }
 
 bool SkkDictModel::removeRows(int row, int count, const QModelIndex &parent) {
-    if (parent.isValid()) {
-        return false;
-    }
+  if (parent.isValid()) {
+    return false;
+  }
 
-    if (count == 0 || row >= m_dicts.size() || row + count > m_dicts.size()) {
-        return false;
-    }
+  if (count == 0 || row >= m_dicts.size() || row + count > m_dicts.size()) {
+    return false;
+  }
 
-    beginRemoveRows(parent, row, row + count - 1);
-    m_dicts.erase(m_dicts.begin() + row, m_dicts.begin() + row + count);
-    endRemoveRows();
+  beginRemoveRows(parent, row, row + count - 1);
+  m_dicts.erase(m_dicts.begin() + row, m_dicts.begin() + row + count);
+  endRemoveRows();
 
-    return true;
+  return true;
 }
 
 QVariant SkkDictModel::data(const QModelIndex &index, int role) const {
-    if (!index.isValid()) {
-        return QVariant();
-    }
-
-    if (index.row() >= m_dicts.size() || index.column() != 0) {
-        return QVariant();
-    }
-
-    switch (role) {
-    case Qt::DisplayRole:
-        return m_dicts[index.row()]["file"];
-    }
+  if (!index.isValid()) {
     return QVariant();
+  }
+
+  if (index.row() >= m_dicts.size() || index.column() != 0) {
+    return QVariant();
+  }
+
+  switch (role) {
+  case Qt::DisplayRole:
+    return m_dicts[index.row()]["file"];
+  case Qt::EditRole:
+    return serialize(m_dicts[index.row()]);
+  }
+  return QVariant();
+}
+
+bool SkkDictModel::setData(const QModelIndex &index, const QVariant &value,
+                           int role) {
+  if (role == Qt::EditRole) {
+    QString dictString = value.toString();
+    QMap<QString, QString> dict = parseLine(dictString);
+    m_dicts[index.row()] = dict;
+    dataChanged(index, index, {role});
+    return true;
+  }
+  return false;
 }
 
 bool SkkDictModel::moveUp(const QModelIndex &currentIndex) {
-    if (currentIndex.row() > 0 && currentIndex.row() < m_dicts.size()) {
-        beginResetModel();
+  if (currentIndex.row() > 0 && currentIndex.row() < m_dicts.size()) {
+    beginResetModel();
 #if (QT_VERSION < QT_VERSION_CHECK(5, 13, 0))
-        m_dicts.swap(currentIndex.row() - 1, currentIndex.row());
+    m_dicts.swap(currentIndex.row() - 1, currentIndex.row());
 #else
-        m_dicts.swapItemsAt(currentIndex.row() - 1, currentIndex.row());
+    m_dicts.swapItemsAt(currentIndex.row() - 1, currentIndex.row());
 #endif
-        endResetModel();
-        return true;
-    }
-    return false;
+    endResetModel();
+    return true;
+  }
+  return false;
 }
 
 bool SkkDictModel::moveDown(const QModelIndex &currentIndex) {
-    if (currentIndex.row() >= 0 && currentIndex.row() + 1 < m_dicts.size()) {
-        beginResetModel();
+  if (currentIndex.row() >= 0 && currentIndex.row() + 1 < m_dicts.size()) {
+    beginResetModel();
 #if (QT_VERSION < QT_VERSION_CHECK(5, 13, 0))
-        m_dicts.swap(currentIndex.row() + 1, currentIndex.row());
+    m_dicts.swap(currentIndex.row() + 1, currentIndex.row());
 #else
-        m_dicts.swapItemsAt(currentIndex.row() + 1, currentIndex.row());
+    m_dicts.swapItemsAt(currentIndex.row() + 1, currentIndex.row());
 #endif
-        endResetModel();
-        return true;
-    }
+    endResetModel();
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 void SkkDictModel::add(const QMap<QString, QString> &dict) {
-    beginInsertRows(QModelIndex(), m_dicts.size(), m_dicts.size());
-    m_dicts << dict;
-    endInsertRows();
+  beginInsertRows(QModelIndex(), m_dicts.size(), m_dicts.size());
+  m_dicts << dict;
+  endInsertRows();
+}
+
+QMap<QString, QString> SkkDictModel::parseLine(const QString &line) {
+  QStringList items = line.split(",");
+  // No matter which type, it should has at least 3 keys.
+  if (items.size() < 3) {
+    return {};
+  }
+
+  QMap<QString, QString> dict;
+  Q_FOREACH (const QString &item, items) {
+    if (!item.contains('=')) {
+      return {};
+    }
+    QString key = item.section('=', 0, 0);
+    QString value = item.section('=', 1, -1);
+
+    if (!SkkDictModel::m_knownKeys.contains(key)) {
+      continue;
+    }
+
+    dict[key] = value;
+  }
+
+  // Inheritance from fcitx5-skk, encoding was optional.
+  if (!dict.keys().contains("encoding")) {
+    dict["encoding"] = "euc-jp";
+  }
+
+  if (dict.keys().contains("file") && dict.keys().contains("type") &&
+      dict.keys().contains("mode") && dict.keys().contains("encoding")) {
+    return dict;
+  }
+  return {};
+}
+
+// Serialize to QString of ',' separated k=v pair.
+// This k=v pair string is to match the configuration reading function of old
+// fcitx, and to be used as QVariant.
+QString SkkDictModel::serialize(const QMap<QString, QString> &dict) {
+  bool first = true;
+  QString result = "";
+  Q_FOREACH (const QString &key, dict.keys()) {
+    if (first) {
+      first = false;
+    } else {
+      result.append(",");
+    }
+    result.append(key);
+    result.append("=");
+    result.append(dict[key]);
+  }
+
+  return result;
 }
 
 } // namespace fcitx
--- gui/dictwidget.cpp
+++ gui/dictwidget.cpp
@@ -1,44 +1,47 @@
 /*
- * SPDX-FileCopyrightText: 2013~2020 CSSlayer <wengxt@gmail.com>
+ * SPDX-FileCopyrightText: <text>2013~2022 CSSlayer <wengxt@gmail.com>, Naoaki Iwakiri
+ * <naokiri@gmail.com></text>
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
  */
 #include "dictwidget.h"
-#include <fcntl.h>
+#include "adddictdialog.h"
+#include "dictmodel.h"
 #include <fcitx-utils/fs.h>
 #include <fcitx-utils/standardpath.h>
 #include <fcitx-utils/stringutils.h>
 #include <fcitxqti18nhelper.h>
-#include "adddictdialog.h"
-#include "dictmodel.h"
+#include <fcntl.h>
 
 namespace fcitx {
 
 SkkDictWidget::SkkDictWidget(QWidget *parent)
     : FcitxQtConfigUIWidget(parent),
       m_ui(std::make_unique<Ui::SkkDictWidget>()) {
-    m_ui->setupUi(this);
-    m_dictModel = new SkkDictModel(this);
-    auto fcitxBasePath = stringutils::joinPath(
-        StandardPath::global().userDirectory(StandardPath::Type::PkgData),
-        "cskk");
-    fs::makePath(fcitxBasePath);
+  m_ui->setupUi(this);
+  m_dictModel = new SkkDictModel(this);
+  auto fcitxBasePath = stringutils::joinPath(
+      StandardPath::global().userDirectory(StandardPath::Type::PkgData),
+      "cskk");
+  fs::makePath(fcitxBasePath);
 
-    m_ui->dictionaryView->setModel(m_dictModel);
+  m_ui->dictionaryView->setModel(m_dictModel);
 
-    connect(m_ui->addDictButton, &QPushButton::clicked, this,
-            &SkkDictWidget::addDictClicked);
-    connect(m_ui->defaultDictButton, &QPushButton::clicked, this,
-            &SkkDictWidget::defaultDictClicked);
-    connect(m_ui->removeDictButton, &QPushButton::clicked, this,
-            &SkkDictWidget::removeDictClicked);
-    connect(m_ui->moveUpDictButton, &QPushButton::clicked, this,
-            &SkkDictWidget::moveUpDictClicked);
-    connect(m_ui->moveDownDictButton, &QPushButton::clicked, this,
-            &SkkDictWidget::moveDownClicked);
+  connect(m_ui->addDictButton, &QPushButton::clicked, this,
+          &SkkDictWidget::addDictClicked);
+  connect(m_ui->defaultDictButton, &QPushButton::clicked, this,
+          &SkkDictWidget::defaultDictClicked);
+  connect(m_ui->removeDictButton, &QPushButton::clicked, this,
+          &SkkDictWidget::removeDictClicked);
+  connect(m_ui->moveUpDictButton, &QPushButton::clicked, this,
+          &SkkDictWidget::moveUpDictClicked);
+  connect(m_ui->moveDownDictButton, &QPushButton::clicked, this,
+          &SkkDictWidget::moveDownClicked);
+  connect(m_ui->editDictButton, &QPushButton::clicked, this,
+          &SkkDictWidget::editDictClicked);
 
-    load();
+  load();
 }
 
 QString SkkDictWidget::title() { return _("Dictionary Manager"); }
@@ -46,52 +49,67 @@ QString SkkDictWidget::title() { return _("Dictionary Manager"); }
 QString SkkDictWidget::icon() { return "cskk"; }
 
 void SkkDictWidget::load() {
-    m_dictModel->load();
-    emit changed(false);
+  m_dictModel->load();
+  emit changed(false);
 }
 
 void SkkDictWidget::save() {
-    m_dictModel->save();
-    emit changed(false);
+  m_dictModel->save();
+  emit changed(false);
 }
 
 void SkkDictWidget::addDictClicked() {
-    AddDictDialog dialog;
-    int result = dialog.exec();
-    if (result == QDialog::Accepted) {
-        m_dictModel->add(dialog.dictionary());
-        emit changed(true);
-    }
+  AddDictDialog dialog;
+  int result = dialog.exec();
+  if (result == QDialog::Accepted) {
+    m_dictModel->add(dialog.dictionary());
+    emit changed(true);
+  }
 }
 
 void SkkDictWidget::defaultDictClicked() {
-    m_dictModel->defaults();
-    emit changed(true);
+  m_dictModel->defaults();
+  emit changed(true);
 }
 
 void SkkDictWidget::removeDictClicked() {
-    if (m_ui->dictionaryView->currentIndex().isValid()) {
-        m_dictModel->removeRow(m_ui->dictionaryView->currentIndex().row());
-        emit changed(true);
-    }
+  if (m_ui->dictionaryView->currentIndex().isValid()) {
+    m_dictModel->removeRow(m_ui->dictionaryView->currentIndex().row());
+    emit changed(true);
+  }
 }
 
 void SkkDictWidget::moveUpDictClicked() {
-    int row = m_ui->dictionaryView->currentIndex().row();
-    if (m_dictModel->moveUp(m_ui->dictionaryView->currentIndex())) {
-        m_ui->dictionaryView->selectionModel()->setCurrentIndex(
-            m_dictModel->index(row - 1), QItemSelectionModel::ClearAndSelect);
-        emit changed(true);
-    }
+  int row = m_ui->dictionaryView->currentIndex().row();
+  if (m_dictModel->moveUp(m_ui->dictionaryView->currentIndex())) {
+    m_ui->dictionaryView->selectionModel()->setCurrentIndex(
+        m_dictModel->index(row - 1), QItemSelectionModel::ClearAndSelect);
+    emit changed(true);
+  }
 }
 
 void SkkDictWidget::moveDownClicked() {
-    int row = m_ui->dictionaryView->currentIndex().row();
-    if (m_dictModel->moveDown(m_ui->dictionaryView->currentIndex())) {
-        m_ui->dictionaryView->selectionModel()->setCurrentIndex(
-            m_dictModel->index(row + 1), QItemSelectionModel::ClearAndSelect);
-        emit changed(true);
-    }
+  int row = m_ui->dictionaryView->currentIndex().row();
+  if (m_dictModel->moveDown(m_ui->dictionaryView->currentIndex())) {
+    m_ui->dictionaryView->selectionModel()->setCurrentIndex(
+        m_dictModel->index(row + 1), QItemSelectionModel::ClearAndSelect);
+    emit changed(true);
+  }
+}
+
+void SkkDictWidget::editDictClicked() {
+  QModelIndex index = m_ui->dictionaryView->currentIndex();
+  QVariant dictValue = m_dictModel->data(index, Qt::EditRole);
+  QMap<QString, QString> dictionary =
+      SkkDictModel::parseLine(dictValue.toString());
+  AddDictDialog dialog;
+  dialog.setDictionary(dictionary);
+  int result = dialog.exec();
+  if (result == QDialog::Accepted) {
+    QString val = SkkDictModel::serialize(dialog.dictionary());
+    m_dictModel->setData(index, val);
+    emit changed(true);
+  }
 }
 
 } // namespace fcitx
