af3dc7c06e22db0f704d014570e3f9ad625d3b99
Naoaki Iwakiri <naokiri@gmail.com>
2022-09-16 00:33:22 +0900

    Version 0.7.0

--- bin/version.sh
+++ bin/version.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Run from repository root and output version from CMakeLists.txt
 # Helper for release automation
+set -e
 cat CMakeLists.txt | sed -n -e "s/project(fcitx5-cskk VERSION \(.*\))/\1/p"
