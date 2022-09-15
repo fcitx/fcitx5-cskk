c617e325bc7ea71122c0cb0e9cd8a5a4a22fddbb
Naoaki Iwakiri <naokiri@gmail.com>
2022-09-16 00:28:22 +0900

    GitHub actions build (#39)
    * Deploy artifact deb package

---
+++ bin/version.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Run from repository root and output version from CMakeLists.txt
+# Helper for release automation
+cat CMakeLists.txt | sed -n -e "s/project(fcitx5-cskk VERSION \(.*\))/\1/p"
